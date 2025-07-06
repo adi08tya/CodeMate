46f5142f5a9c826fa0805308e515b4ea17fa6f25
adi08tya <adityaparashar966@gmail.com>
2025-07-06 19:37:01 +0530

    Dynamically create a new user based on the request body

--- src/app.js
+++ src/app.js
@@ -1,28 +1,23 @@
 const express = require("express");
-const connectDB = require("./Config/database.js")
+const connectDB = require("./Config/database.js");
 const { adminAuth, userAuth } = require("./middlewares/auth.middleware.js");
 const app = express();
+app.use(express.json());
 const User = require("./models/user.js");
-app.post("/signup",async(req,res)=>{
-    // Creating a new instance of user model
-    const user = new User({
-      firstName: "Adi",
-      lastName: "Parashar",
-      emailId: "Aditya@parashar.com",
-      password: "123456",
-      age: 22,
-      gender: "Male",
-    });
-    try {
-      await user.save();
-      res.send("User created succesfully");
-    } catch (error) {
-      res.status(400).send("Error creating user: " + error.message);
-    }
+app.post("/signup", async (req, res) => {
+  // Dynamically create a new user based on the request body
+   const user = new User(req.body)
+  try {
+    await user.save();
+    res.send("User created succesfully");
+  } catch (error) {
+    res.status(400).send("Error creating user: " + error.message);
+  }
 });
 
 connectDB()
-  .then(() =>{console.log("MongoDB connected successfully")
+  .then(() => {
+    console.log("MongoDB connected successfully");
     app.listen(3000, () => {
       console.log("server is successfully listening to port number 3000");
     });
